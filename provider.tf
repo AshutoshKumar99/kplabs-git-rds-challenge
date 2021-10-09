e547b5de60c91eeed60f57e632ef0fbb976b64db
Ashutosh Kumar <ashutoshkumar101094@gmail.com>
2021-10-09 13:33:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA6IFD7ZT7J5JRXIIB"
-  secret_key = "HTuX8orsuJCHTE2l/1o783K67jdVQoN/O9xaLoqc"
+  access_key = "your access key"
+  secret_key = "your secret access key"
 }
